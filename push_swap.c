671277e0c3d09d6c2cdf952c622fec3db1b4f79e
Abdelali <abde69436@gmail.com>
2024-04-01 01:03:57 +0000

    main

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:14:50 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/03/30 17:45:04 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/03/31 22:33:29 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,67 +23,14 @@ int	main(int argc, char **argv)
 	if (argc == 1 || (argc == 2 && argv[1][0] == '\0'))
 		return (1);
 	else if (argc == 2)
+	{
 		argv = ft_split(argv[1], ' ');
-	stack_init(&a, argv);
-	int	i = 0;
-	t_stack *tmp = a;
-	while (tmp)
-	{
-		tmp = tmp->next;
-		i++;
+		stack_init(&a, argv);
 	}
-	tmp = a;
-	while (i > 0)
-	{
-		int	n;
-		while (tmp->next)
-		{
-			if (tmp->value > tmp->next->value)
-			{
-				n = tmp->value;
-				tmp->value = tmp->next->value;
-				tmp->next->value = n;
-			}
-			tmp = tmp->next;
-		}
-		tmp = a;
-		i--;
-	}
-	tmp = a;
-	while (tmp)
-	{
-		printf("%d\n", tmp->value);
-		tmp = tmp->next;
-	}
+	else
+		stack_init(&a, argv + 1);
+	if (is_sorted(&a) == true)
+		return (0);
+	else
+		sort_stack(&a);
 }
-
-/* t_stack* newNode(int value) {
-    t_stack* node = (t_stack*)malloc(sizeof(t_stack));
-    if (node == NULL) {
-        printf("Memory allocation failed\n");
-    }
-    node->value = value;
-    node->next = NULL;
-    return node;
-}
-
-int main() {
-    t_stack *node1 = newNode(100);
-    t_stack *node2 = newNode(90);
-    t_stack *node3 = newNode(9);
-    t_stack *node4 = newNode(0);
-    node1->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    
-    reverse_rotate(&node1);
-
-    t_stack *tmp = node1;
-    while (tmp != NULL)
-    {
-        printf("%d\n", tmp->value);
-        tmp = tmp->next;
-    }
-    
-    return 0;
-} */
