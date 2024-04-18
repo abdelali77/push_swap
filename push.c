36cc904af447671821cfd216f2a59993c622f02e
Abdelali <abde69436@gmail.com>
2024-04-18 21:29:03 +0100

    update

--- push.c
+++ push.c
@@ -6,41 +6,34 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 18:18:02 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/03/27 22:41:49 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:55:44 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_lstadd_front(t_stack **lst, t_stack *new)
+void	ft_push(t_stack **lst, t_stack **new)
 {
-	if (lst && new)
-	{
-		new->next = *lst;
-		*lst = new;
-	}
+	t_stack	*tmp;
+
+	tmp = *lst;
+	*lst = (*lst)->next;
+	tmp->next = *new;
+	*new = tmp;
 }
 
 void	_pa(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*tmp;
-
 	if (*stack_b == NULL)
 		return ;
-	tmp = *stack_b;
-	*stack_b = (*stack_b)->next;
-	ft_lstadd_front(stack_a, tmp);
+	ft_push(stack_b, stack_a);
 	write(1, "pa\n", 3);
 }
 
 void	_pb(t_stack **stack_a, t_stack **stack_b)
 {
-	t_stack	*tmp;
-
 	if (*stack_a == NULL)
 		return ;
-	tmp = *stack_a;
-	*stack_a = (*stack_a)->next;
-	ft_lstadd_front(stack_b, tmp);
+	ft_push(stack_a, stack_b);
 	write(1, "pb\n", 3);
 }
