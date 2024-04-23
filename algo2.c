63c343312dc376cb925303fe3f5b35ac3734c1e6
Abdelali <abde69436@gmail.com>
2024-04-23 11:31:59 +0100

    main

--- algo2.c
+++ algo2.c
@@ -6,7 +6,7 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 13:23:54 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/04/20 16:10:27 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/04/23 11:14:00 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,63 @@ void	indexing_stack(t_stack *stack)
 		}
 		x = x->next;
 	}
+}
+
+size_t	distance_from_top(t_stack *stack, t_stack *element)
+{
+	t_stack	*tmp;
+	int		distance;
+
+	tmp = stack;
+	distance = 0;
+	while (tmp && tmp != element)
+	{
+		tmp = tmp->next;
+		distance++;
+	}
+	return (distance);
+}
+
+void	sort_stack_extra(t_stack **a, t_stack **b)
+{
+	while (*b)
+	{
+		re_index(*b);
+		while (*b != max_el(b))
+		{
+			if (max_el(b)->index < stack_size(b) / 2)
+				_rb(b);
+			else
+				_rrb(b);
+		}
+		_pa(b, a);
+	}
+}
+
+void	sort_stack(t_stack **a, t_stack **b, int range)
+{
+	unsigned long	start;
+	unsigned long	end;
+
+	start = 0;
+	end = range;
+	while (*a)
+	{
+		if ((*a)->index >= start && (*a)->index <= end)
+		{
+			_pb(a, b);
+			start++;
+			end++;
+		}
+		else if ((*a)->index < start)
+		{
+			_pb(a, b);
+			_rb(b);
+			start++;
+			end++;
+		}
+		else
+			_ra(a);
+	}
+	sort_stack_extra(a, b);
 }
