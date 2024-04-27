23867a42b68ff64e05cb6305f09ef66a7f6c13f0
Abdelali <abde69436@gmail.com>
2024-04-27 15:22:31 +0100

    bonus

--- bonus/header_bonus.h
+++ bonus/header_bonus.h
@@ -6,14 +6,14 @@
 /*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:31:49 by abmahfou          #+#    #+#             */
-/*   Updated: 2024/04/26 15:33:09 by abmahfou         ###   ########.fr       */
+/*   Updated: 2024/04/27 11:33:14 by abmahfou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_BONUS_H
-#define HEADER_BONUS_H
+# define HEADER_BONUS_H
 
-#include "../push_swap.h"
-#include "./get_next_line/get_next_line.h"
+# include "../push_swap.h"
+# include "./get_next_line/get_next_line.h"
 
 #endif
