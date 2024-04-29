c20c4360169757500c8d0b629bed0985a83e76cf
Abdelali <abde69436@gmail.com>
2024-04-29 21:49:12 +0100

    free

---
+++ free_stack.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_stack.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abmahfou <abmahfou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 20:10:47 by abmahfou          #+#    #+#             */
+/*   Updated: 2024/04/29 20:40:25 by abmahfou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_free(t_stack *stack)
+{
+	t_stack	*tmp;
+
+	while (stack != NULL)
+	{
+		tmp = stack;
+		stack = stack->next;
+		free(tmp);
+	}
+}
+
+void	free_mem(char **split)
+{
+	int	n;
+
+	n = 0;
+	while (split[n])
+	{
+		free(split[n]);
+		n++;
+	}
+	free(split);
+}
